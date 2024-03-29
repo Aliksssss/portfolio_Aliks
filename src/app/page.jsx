a2ba656b552678c13c191afbda1afd2e6c89edbe
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-03-29 06:39:35 -0700

    added stuffs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
-const text = "Creating Fashion";
+const text = "Welcome To Ps SH";
 
 const HomePage = () => {
   return (
