9b0d3fadd879f08c02da31255d0ae46ff7a3800a
Jordyn Jensen <jordyneajensen@gmail.com>
2024-05-31 14:52:09 -0500

    modified css

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -53,7 +53,7 @@ const PortfolioPage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-4xl md:text-8xl text-center">
           My Works
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
@@ -64,18 +64,20 @@ const PortfolioPage = () => {
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
                 key={item.id}
               >
-                <div className="flex flex-col gap-8 text-white">
-                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                <div className="flex flex-col gap-4 md:gap-8 text-white p-4 md:p-8 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
+                  <h1 className="text-2xl md:text-4xl lg:text-6xl xl:text-8xl font-bold">
                     {item.title}
                   </h1>
-                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
-                    <Image src={item.img} alt="" fill />
+                  <div className="relative w-full h-56 md:h-64 lg:h-80">
+                    <Image src={item.img} alt={item.title} layout="fill" objectFit="cover" />
                   </div>
-                  <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                  <p className="text-sm md:text-lg lg:text-xl">
                     {item.desc}
                   </p>
-                  <Link href={item.link} className="flex justify-end">
-                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">See Demo</button>
+                  <Link href={item.link}>
+                    <button className="p-2 md:p-4 lg:p-6 text-sm md:text-lg lg:text-xl bg-white text-gray-600 font-semibold rounded hover:bg-gray-200">
+                      See Demo
+                    </button>
                   </Link>
                 </div>
               </div>
@@ -83,14 +85,14 @@ const PortfolioPage = () => {
           </motion.div>
         </div>
       </div>
-      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-        <h1 className="text-8xl">Do you have a project?</h1>
+      <div className="w-screen h-screen flex flex-col gap-8 md:gap-16 items-center justify-center text-center">
+        <h1 className="text-4xl md:text-8xl">Do you have a project?</h1>
         <div className="relative">
           <motion.svg
             animate={{ rotate: 360 }}
             transition={{ duration: 8, ease: "linear", repeat: Infinity }}
             viewBox="0 0 300 300"
-            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+            className="w-32 h-32 md:w-64 md:h-64 lg:w-96 lg:h-96"
           >
             <defs>
               <path
@@ -99,15 +101,14 @@ const PortfolioPage = () => {
               />
             </defs>
             <text fill="#000">
-              <textPath xlinkHref="#circlePath" className="text-xl">
+              <textPath xlinkHref="#circlePath" className="text-sm md:text-lg lg:text-xl">
                 Front-end, Backend, & Cloud Developer
               </textPath>
             </text>
           </motion.svg>
-          <Link
-            href="/contact"><button
-           className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center hover:bg-black hover:text-white">
-            Hire Me
+          <Link href="/contact">
+            <button className="w-12 h-12 md:w-24 md:h-24 lg:w-32 lg:h-32 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center hover:bg-gray-800">
+              Hire Me
             </button>
           </Link>
         </div>
